287b14fe46ee084105c8231114172ef756338ee1
Suresh Maidaragi <suresh.maidaragi@fridaymediagroup.com>
2018-11-09 17:11:28 +0530

    signed apk generated

--- DemoApp/app/src/main/java/maida/com/demoapp/model/CountryModel.kt
+++
@@ -1,6 +0,0 @@
-package maida.com.demoapp.model
-
-data class CountryModel(
-        val title: String,
-        val rows: List<RowModel>
-)
--- DemoApp/app/src/main/java/maida/com/demoapp/model/RowModel.kt
+++
@@ -1,7 +0,0 @@
-package maida.com.demoapp.model
-
-data class RowModel(
-        val title: String,
-        val description: String,
-        val imageHref: Any
-)
